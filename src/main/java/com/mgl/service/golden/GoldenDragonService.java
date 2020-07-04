a287fc4e74bbd11d188e7db1b3981fae80d35054
zhangqi <zqloner@163.com>
2020-07-04 09:37:48 +0800

    读取csv,金龙数据抓取

---
+++ src/main/java/com/mgl/service/golden/GoldenDragonService.java
@@ -0,0 +1,16 @@
+package com.mgl.service.golden;
+
+import com.mgl.bean.golden.GoldenDragon;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 金龙数据实时抓取 服务类
+ * </p>
+ *
+ * @author zhangqi
+ * @since 2020-07-03
+ */
+public interface GoldenDragonService extends IService<GoldenDragon> {
+
+}
---
+++ src/main/java/com/mgl/service/golden/impl/GoldenDragonServiceImpl.java
@@ -0,0 +1,20 @@
+package com.mgl.service.golden.impl;
+
+import com.mgl.bean.golden.GoldenDragon;
+import com.mgl.dao.golden.GoldenDragonMapper;
+import com.mgl.service.golden.GoldenDragonService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 金龙数据实时抓取 服务实现类
+ * </p>
+ *
+ * @author zhangqi
+ * @since 2020-07-03
+ */
+@Service
+public class GoldenDragonServiceImpl extends ServiceImpl<GoldenDragonMapper, GoldenDragon> implements GoldenDragonService {
+
+}
